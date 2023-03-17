a2170dfa3386756d885fcd91d79374dc33770b3f
Keerthan <kkeerthan@gmail.com>
2023-03-16 23:39:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ fig = px.scatter_mapbox(
     labels={"color": "Locations"},
 )
 fig.update_geos(center=dict(lat=dataframe.iloc[0][2], lon=dataframe.iloc[0][3]))
-fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(mapbox_style="carto-positron")
 
 # Show the figure
 st.plotly_chart(fig, use_container_width=True)
